1d06c0477b883b7ea526495dfa9f1e68c003f683
Mohammad Almosallam <moh.almusallam4@gmail.com>
2024-05-17 21:18:01 +0300

    cloud

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Using fetch
+    fetch("https://api.restful-api.dev/objects")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +41,17 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <h2>Data from API:</h2>
+          <ul>
+            {loading && <div>LOADING LOADING LOADING.....</div>}
+            {data.map((item) => (
+              <li key={item.id}>
+                <strong>{item.name}</strong>: {item.description}
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </div>
   );
